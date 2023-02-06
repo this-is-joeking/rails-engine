2631b35d71b5c3525e60f120703587a8b053d1ac
Joe King <j.king92@gmail.com>
2023-02-06 15:09:04 -0700

    Initial commit

---
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe 'Merchants API' do
+  describe 'get all merchants' do
+    it 'sends all the merchants' do
+      create_list(:merchant, 5)
+
+      get '/api/v1/merchants'
+
+      expect(response).to be_successful
+
+      merchants = JSON.parse(response.body, symbolize_names: true)
+      merchants.each do |merchant|
+        expect(merchant).to have_key(:id)
+        expect(merchant[:id]).to be_a Integer
+
+        expect(merchant).to have_key(:name)
+        expect(merchant[:name]).to be_a String
+      end
+    end
+  end
+
+  describe 'get one merchant' do
+    it 'sends 1 merchant' do
+      id = create(:merchant).id
+
+      get "/api/v1/merchants/#{id}"
+
+      merchant = JSON.parse(response.body, symbolize_names: true)
+
+      expect(merchant).to have_key(:id)
+      expect(merchant[:id]).to be_a Integer
+      expect(merchant).to have_key(:name)
+      expect(merchant[:name]).to be_a String
+    end
+  end
+end
