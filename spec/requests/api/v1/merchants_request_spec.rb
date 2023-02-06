91ca231e96c90c1395c9b1a22def9dbf7c01a5b9
Joe King <j.king92@gmail.com>
2023-02-06 16:58:23 -0700

    feat: Add the endpoint for getting all of a merchants items

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -43,4 +43,43 @@ RSpec.describe 'Merchants API' do
       expect(merchant[:attributes][:name]).to be_a String
     end
   end
+
+  describe 'get all items for a merchant' do
+    it 'sends all items for given merchant' do
+      id = create(:merchant).id
+      create_list(:item, 10)
+
+      create_list(:merchant, 5)
+      create_list(:item, 10)
+      
+      get "/api/v1/merchants/#{id}/items"
+
+      expect(response).to be_successful
+
+      items_data = JSON.parse(response.body, symbolize_names: true)
+
+      expect(items_data).to have_key(:data)
+
+      items_data[:data].each do |item|
+        expect(item).to have_key(:id)
+        expect(item[:id]).to be_a String
+        expect(item[:id].to_i).to be_a Integer
+
+        expect(item).to have_key(:attributes)
+
+        expect(item[:attributes]).to have_key(:name)
+        expect(item[:attributes][:name]).to be_a String
+
+        expect(item[:attributes]).to have_key(:description)
+        expect(item[:attributes][:description]).to be_a String
+
+        expect(item[:attributes]).to have_key(:unit_price)
+        expect(item[:attributes][:unit_price]).to be_a Float
+
+        expect(item[:attributes]).to have_key(:merchant_id)
+        expect(item[:attributes][:merchant_id]).to be_a Integer
+        expect(item[:attributes][:merchant_id]).to eq(id)
+      end
+    end
+  end
 end
