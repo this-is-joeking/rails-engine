03f2543e4a63a17c40dacbb15af06e7f5ba4c2a8
Joe King <j.king92@gmail.com>
2023-02-07 15:00:32 -0700

    feat: Add message key to error serializer

--- app/serializers/error_serializer.rb
+++ app/serializers/error_serializer.rb
@@ -1,6 +1,7 @@
 class ErrorSerializer
   def self.errors(model)
-    {
+    { 
+      "message": "your query could not be completed",
       "errors": model.errors.full_messages.map do |error|
         { 'detail' => error }
       end
