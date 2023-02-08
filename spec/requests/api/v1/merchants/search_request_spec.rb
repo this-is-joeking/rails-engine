68890ee6b7596614bf6a67b16e778ffbfbe3e007
Joe King <j.king92@gmail.com>
2023-02-08 13:34:37 -0700

    linting: ran rubocop -a

--- spec/requests/api/v1/merchants/search_request_spec.rb
+++ spec/requests/api/v1/merchants/search_request_spec.rb
@@ -13,13 +13,11 @@ RSpec.describe 'find all merchants' do
 
     expect(merchants_data).to have_key(:data)
     merchants_data[:data].each do |merchant|
-      expect(merchant.keys.sort).to eq([:id, :type, :attributes].sort)
+      expect(merchant.keys.sort).to eq(%i[id type attributes].sort)
       expect(merchant[:id]).to be_a String
       expect(merchant[:type]).to eq 'merchant'
       expect(merchant[:attributes]).to have_key(:name)
       expect(merchant[:attributes][:name]).to be_a String
     end
-    
   end
-  
 end
