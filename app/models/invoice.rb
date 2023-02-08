d82a54d8234fcc0c43e8829bc204b5fe165787b6
Joe King <j.king92@gmail.com>
2023-02-08 14:05:01 -0700

    removed comments

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -3,8 +3,4 @@
 class Invoice < ApplicationRecord
   belongs_to :customer
   has_many :invoice_items, dependent: :destroy
-  # has_many :items, through: :invoice_items
-  # has_many :transactions, dependent: :destroy
-  # has_many :merchants, through: :items
-  # enum status: { 'in progress' => 0, completed: 1, cancelled: 2 }
 end
