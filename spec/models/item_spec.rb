3531fb64d8d19f20ef0adffd371d8406f65353db
Joe King <j.king92@gmail.com>
2023-02-08 13:51:20 -0700

    test: Add relationship and validation tests for item

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,6 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
+  describe 'relationships' do
+    it { should belong_to :merchant }
+    it { should have_many :invoice_items }
+    it { should have_many(:invoices).through(:invoice_items) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:description) }
+    it { should validate_presence_of(:unit_price) }
+    it { should validate_numericality_of(:unit_price) }
+  end
   describe '#find_item_by_name()' do
     it 'returns the first single item in case insensitive search' do
       merch_id = create(:merchant).id
